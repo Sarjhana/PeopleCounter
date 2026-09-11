376e649006d5d064173cdab6b036eb9553c6bb14
Sarjhana <sarjhur@gmail.com>
2026-09-11 18:55:36 +0100

    Initial commit

---
+++ detector.py
@@ -0,0 +1,130 @@
+import cv2
+import imutils
+import numpy as np
+import argparse
+
+HOGCV = cv2.HOGDescriptor()
+HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+def non_max_suppression_fast(boxes, overlapThresh):
+    try:
+        if len(boxes) == 0:
+            return []
+
+        if boxes.dtype.kind == "i":
+            boxes = boxes.astype("float")
+
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+
+        while len(idxs) > 0:
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            overlap = (w * h) / area[idxs[:last]]
+
+            idxs = np.delete(idxs, np.concatenate(([last],
+                                                   np.where(overlap > overlapThresh)[0])))
+
+        return boxes[pick].astype("int")
+    except Exception as e:
+        print("Exception occurred in non_max_suppression : {}".format(e))
+
+def detect(frame):
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bounding_box_cordinates, weights =  HOGCV.detectMultiScale(gray, winStride = (4, 4), padding = (8, 8), scale = 1.03)
+    
+    rects = non_max_suppression_fast(bounding_box_cordinates, 0.7)
+
+    person = 1
+    for x,y,w,h in rects:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+        person += 1
+    
+    cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    cv2.imshow('output', frame)
+
+    return frame
+
+def fullbodyDetect(frame):
+    cascPath = "haarcascade_fullbody.xml"
+
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    person = 1
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+        person += 1
+
+    cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    cv2.imshow('output', frame)
+
+    return frame
+
+    
+def detectByPathVideo(path, writer):
+
+    video = cv2.VideoCapture(path)
+    check, frame = video.read()
+    if check == False:
+        print('Video Not Found. Please Enter a Valid Path (Full path of Video Should be Provided).')
+        return
+
+    print('Detecting people...')
+    while video.isOpened():
+        #check is True if reading was successful 
+        check, frame =  video.read()
+
+        if check:
+            frame = imutils.resize(frame , width=min(800,frame.shape[1]))
+            frame = fullbodyDetect(frame)
+            
+            if writer is not None:
+                writer.write(frame)
+            
+            key = cv2.waitKey(1)
+            if key== ord('q'):
+                break
+        else:
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    HOGCV = cv2.HOGDescriptor()
+    HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    video_path = ""  # add path to your test mp4 file here
+    writer = None
+    detectByPathVideo(video_path, writer)
---
+++ trial.py
@@ -0,0 +1,4 @@
+o = set([1,2,3,4])
+r = set([1,2,7,8])
+
+print(o.intersection(r))
